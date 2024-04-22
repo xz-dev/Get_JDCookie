8e009ed383627cfb14c9d89694c7cd87e6fd802f
xz-dev <xiangzhedev@gmail.com>
2024-04-23 02:08:35 +0800

    feat: replace pyppeteer with playwright

--- Get-JDCookie.py
+++ Get-JDCookie.py
@@ -2,53 +2,58 @@
 
 # -*- coding:utf-8 -*-
 import asyncio
-from random import random
-from socket import timeout
-from time import sleep
 import pyperclip
 import os
-from pyppeteer import launch
+from playwright.async_api import async_playwright
 
 
 def find_cookie(cookies):
-    """提取pt_key和pt_pin
-    """
-    for item in cookies.split('; '):
-        if 'pt_pin' in item:
+    """提取pt_key和pt_pin"""
+    for item in cookies.split("; "):
+        if "pt_pin" in item:
             pt_pin = item
-        if 'pt_key' in item:
+        if "pt_key" in item:
             pt_key = item
-    jd_cookie = pt_pin+';'+pt_key+';'
-    pyperclip.copy(jd_cookie)   # 拷贝JDcookie到剪切板
+    jd_cookie = pt_pin + ";" + pt_key + ";"
+    pyperclip.copy(jd_cookie)  # 拷贝JDcookie到剪切板
     print("Cookie:", jd_cookie)
     print("已拷贝Cookie到剪切板、直接黏贴即可。")
     # return jd_cookie
-    os.system('pause')  # 按任意键继续
+    os.system("pause")  # 按任意键继续
+
 
 async def main():
-    """使用pyppeteer库来登录京东、并获取cookie
-    """
-    print('请在弹出的网页中登录账号、推荐使用账户短信验证码的形式登录。')
-    browser = await launch(headless=False, dumpio=True, autoClose=False,
-                           args=['--no-sandbox', '--window-size=1000,800', '--disable-infobars'])   # 进入有头模式
-    context = await browser.createIncognitoBrowserContext() # 隐身模式
-    page = await context.newPage()           # 打开新的标签页
-    await page.setViewport({'width': 1000, 'height': 800})      # 页面大小一致
-    await page.goto('https://home.m.jd.com/myJd/newhome.action',{'timeout': 1000*60}) # 访问主页、增加超时解决Navigation Timeout Exceeded: 30000 ms exceeded报错
+    """使用pyppeteer库来登录京东、并获取cookie"""
+    print("请在弹出的网页中登录账号、推荐使用账户短信验证码的形式登录。")
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(
+            headless=False,
+            args=["--no-sandbox", "--window-size=1000,800", "--disable-infobars"],
+        )  # 进入有头模式
+        context = await browser.new_context()  # 隐身模式
+        page = await context.new_page()  # 打开新的标签页
+        await page.set_viewport_size({"width": 1000, "height": 800})  # 页面大小一致
+        await page.goto(
+            "https://home.m.jd.com/myJd/newhome.action", timeout=1000 * 60
+        )  # 访问主页、增加超时解决Navigation Timeout Exceeded: 30000 ms exceeded报错
 
-    await page.waitFor(1000)
-    elm = await page.waitForXPath('//*[@id="myHeader"]',timeout=0)  # 通过判断用户头像是否存在来确定登录状态
-    if elm:
-        cookie = await page.cookies()
+        await page.wait_for_timeout(1000)
+        elm = page.locator(
+            '//*[@id="myHeader"]'
+        )  # 通过判断用户头像是否存在来确定登录状态
+        while True:
+            if await elm.is_visible():
+                break
+        cookie = await context.cookies()
         # print(cookie)
         # 格式化cookie
         cookies_temp = []
         for i in cookie:
-            cookies_temp.append('{}={}'.format(i["name"], i["value"]))
-        cookies = '; '.join(cookies_temp)
+            cookies_temp.append("{}={}".format(i["name"], i["value"]))
+        cookies = "; ".join(cookies_temp)
         find_cookie(cookies)
-    # print("cookies:{}".format(await page.cookies()))
+        # print("cookies:{}".format(await page.cookies()))
 
 
-if __name__== "__main__":
-    asyncio.get_event_loop().run_until_complete(main()) #调用
+if __name__ == "__main__":
+    asyncio.run(main())  # 调用

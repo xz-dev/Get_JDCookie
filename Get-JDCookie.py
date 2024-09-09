d1f32e05d52c7675a6714777b2c994d7518009a1
xz-dev <xiangzhedev@gmail.com>
2024-09-09 21:42:54 +0800

    fix: fix cookies detect

--- Get-JDCookie.py
+++ Get-JDCookie.py
@@ -3,26 +3,27 @@
 # -*- coding:utf-8 -*-
 import asyncio
 import pyperclip
-import os
 from playwright.async_api import async_playwright
 
 
-def find_cookie(cookies):
+def find_cookie(cookies) -> str:
     """提取pt_key和pt_pin"""
-    for item in cookies.split("; "):
-        if "pt_pin" in item:
-            pt_pin = item
-        if "pt_key" in item:
-            pt_key = item
+    pt_pin = None
+    pt_key = None
+    for key, value in cookies.items():
+        if key == "pt_pin":
+            pt_pin = value
+        elif key == "pt_key":
+            pt_key = value
     if not pt_pin or not pt_key:
         print("cookies:", cookies)
         print("提取失败，请手动提取pt_key和pt_pin")
-        return
-    jd_cookie = pt_pin + ";" + pt_key + ";"
+        return None
+    jd_cookie = f"pt_pin={pt_pin};pt_key={pt_key};"
     pyperclip.copy(jd_cookie)  # 拷贝JDcookie到剪切板
     print("Cookie:", jd_cookie)
     print("已拷贝Cookie到剪切板、直接黏贴即可。")
-    # return jd_cookie
+    return jd_cookie
 
 
 async def main():
@@ -37,25 +38,21 @@ async def main():
         page = await context.new_page()  # 打开新的标签页
         await page.set_viewport_size({"width": 1000, "height": 800})  # 页面大小一致
         await page.goto(
-            "https://home.m.jd.com/myJd/newhome.action", timeout=1000 * 60
+            "https://home.m.jd.com", timeout=1000 * 60
         )  # 访问主页、增加超时解决Navigation Timeout Exceeded: 30000 ms exceeded报错
 
-        await page.wait_for_timeout(1000)
-        elm = page.locator(
-            '//*[@id="myHeader"]'
-        )  # 通过判断用户头像是否存在来确定登录状态
         while True:
-            if await elm.is_visible():
+            cookie = await context.cookies()
+            # print(cookie)
+            # 格式化cookie
+            cookies = {}
+            for i in cookie:
+                cookies[i["name"]] = i["value"]
+            if find_cookie(cookies):
                 break
-            await asyncio.sleep(0.1)
-        cookie = await context.cookies()
-        # print(cookie)
-        # 格式化cookie
-        cookies_temp = []
-        for i in cookie:
-            cookies_temp.append("{}={}".format(i["name"], i["value"]))
-        cookies = "; ".join(cookies_temp)
-        find_cookie(cookies)
+            else:
+                print("未提取到cookie，重试中...")
+                await page.wait_for_timeout(1000)
         await context.close()
         await browser.close()
         # print("cookies:{}".format(await page.cookies()))
